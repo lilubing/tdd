f697ca1f9cd375fad9b8f6aab8f30c13b033957c
8366419@qq.com <8366419@qq.com>
2022-11-09 06:37:54 +0800

    39-4

--- 02.restful/src/test/java/llb/tdd/di/ASpike.java
+++ 02.restful/src/test/java/llb/tdd/di/ASpike.java
@@ -1,15 +1,17 @@
 package llb.tdd.di;
 
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.WebApplicationException;
-import jakarta.ws.rs.core.Application;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.container.ResourceContext;
+import jakarta.ws.rs.core.*;
 import jakarta.ws.rs.ext.*;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
@@ -37,11 +39,14 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
-  * https://github.com/wyyl1/geektime-tdd-framework
- * https://github.com/Antinomy/geektime-tdd-practice
- * https://github.com/lenwind/TDD-Learn
- * https://github.com/wyyl1/geektime-tdd-framework
- * https://github.com/Antinomy/geektime-tdd-practice
+  * https://github.com/wyyl1/geektime-tdd-framework/blob/master/restful/src/test/java/geektime/tdd/rest/ASpike.java
+ * https://github.com/Antinomy/geektime-tdd-practice/tree/master/tdd-rest/src/test/java/geektime/tdd/rest
+ * https://github.com/lenwind/TDD-Learn/blob/main/web-service/src/test/java/pers/lenwind/rest/ASpike.java
+ * https://github.com/wyyl1/geektime-tdd-framework/blob/master/restful/src/test/java/geektime/tdd/rest/ASpike.java
+ *
+ * 38-5
+ *
+ *
   * @Author LiLuBing
   * @Date 2022/11/8 下午8:29
   * @Param  * @param null
@@ -74,32 +79,36 @@ public class ASpike {
 		HttpRequest request = HttpRequest.newBuilder(new URI("http://localhost:8080/")).GET().build();
 		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-		assertEquals("test", response.body());
+		assertEquals("prefixprefixtest", response.body());
 	}
 
 	static class ResourceServlet extends HttpServlet {
-
-		private Application application;
-
+		private final Context context;
+		private TestApplication application;
 		private Providers providers;
 
-		public ResourceServlet(Application application, Providers providers) {
+		public ResourceServlet(TestApplication application, Providers providers) {
 			this.application = application;
 			this.providers = providers;
+
+			context = application.getContext();
 		}
 
 		@Override
 		protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 			Stream<Class<?>> rootResources = application.getClasses().stream().filter(c -> c.isAnnotationPresent(Path.class));
-			Object result = dispatch(req, rootResources);
+
+			ResourceContext rc = application.createResourceContext(req, resp);
+
+			Object result = dispatch(req, rootResources, rc);
 			MessageBodyWriter<Object> writer = (MessageBodyWriter<Object>) providers.getMessageBodyWriter(result.getClass(), null, null, null);
 			writer.writeTo(result, null, null, null, null, null, resp.getOutputStream());
 		}
 
-		Object dispatch(HttpServletRequest req, Stream<Class<?>> rootResources) {
+		Object dispatch(HttpServletRequest req, Stream<Class<?>> rootResources, ResourceContext rc) {
 			try {
 				Class<?> rootClass = rootResources.findFirst().get();
-				Object rootResource = rootClass.getConstructor().newInstance();
+				Object rootResource = rc.initResource(context.get(ComponentRef.of(rootClass)).get());
 				Method method = Arrays.stream(rootClass.getMethods()).filter(m -> m.isAnnotationPresent(GET.class)).findFirst().get();
 				return method.invoke(rootResource);
 			} catch (Exception e) {
@@ -109,37 +118,68 @@ public class ASpike {
 	}
 
 	static class TestApplication extends Application {
+		private final Context context;
+
 		@Override
 		public Set<Class<?>> getClasses() {
 			return Set.of(TestResource.class, StringMessageBodyWriter.class);
 		}
-	}
 
-	static class TestProviders implements Providers {
-		private List<MessageBodyWriter> writers;
-		private Application application;
+		public Config getConfig() {
+			return new Config() {
+				@Named("prefix")
+				public String name = "prefix";
+			};
+		}
 
-		public TestProviders(Application application) {
-			this.application = application;
+		public Context getContext() {
+			return context;
+		}
+
+		public TestApplication() {
 			ContextConfig config = new ContextConfig();
+			config.from(getConfig());
 
-			List<Class<?>> writerClasses = this.application.getClasses().stream().filter(MessageBodyWriter.class::isAssignableFrom).toList();
+			List<Class<?>> writerClasses = this.getClasses().stream().filter(MessageBodyWriter.class::isAssignableFrom).toList();
 
 			for (Class writerClass : writerClasses) {
 				config.component(writerClass, writerClass);
 			}
 
-			Context context = config.getContext();
-			writers = (List<MessageBodyWriter>) writerClasses.stream().map(c -> context.get(ComponentRef.of(c)).get()).toList();
+			List<Class<?>> rootResources = this.getClasses().stream().filter(c -> c.isAnnotationPresent(Path.class)).toList();
+			for (Class rootResource : rootResources) {
+				config.component(rootResource, rootResource);
+			}
 
-			writers = (List<MessageBodyWriter>) writerClasses.stream()
-					.map(c -> {
-						try {
-							return c.getConstructor().newInstance();
-						} catch (Exception e) {
-							throw new RuntimeException(e);
-						}
-					}).toList();
+			context = config.getContext();
+		}
+
+		public ResourceContext createResourceContext(HttpServletRequest request, HttpServletResponse response) {
+			return new ResourceContext() {
+				@Override
+				public <T> T getResource(Class<T> resourceClass) {
+					return null;
+				}
+
+				@Override
+				public <T> T initResource(T resource) {
+					return resource;
+				}
+			};
+		}
+	}
+
+	static class TestProviders implements Providers {
+		private List<MessageBodyWriter> writers;
+		private TestApplication application;
+
+		public TestProviders(TestApplication application) {
+			this.application = application;
+
+			List<Class<?>> writerClasses = this.application.getClasses().stream().filter(MessageBodyWriter.class::isAssignableFrom).toList();
+
+			Context context = application.getContext();
+			writers = (List<MessageBodyWriter>) writerClasses.stream().map(c -> context.get(ComponentRef.of(c)).get()).toList();
 		}
 
 		@Override
@@ -167,6 +207,10 @@ public class ASpike {
 	@Provider
 	static class StringMessageBodyWriter implements MessageBodyWriter<String> {
 
+		@Inject
+		@Named("prefix")
+		String prefix;
+
 		public StringMessageBodyWriter() {
 		}
 
@@ -178,6 +222,7 @@ public class ASpike {
 		@Override
 		public void writeTo(String s, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
 			PrintWriter writer = new PrintWriter(entityStream);
+			writer.write(prefix);
 			writer.write(s);
 			writer.flush();
 		}
@@ -185,12 +230,23 @@ public class ASpike {
 
 	@Path("/test")
 	static class TestResource {
+		@Inject
+		@Named("prefix")
+		String prefix;
+
+		@QueryParam("q")
+		String q;
+
 		public TestResource() {
 		}
 
 		@GET
 		public String get() {
-			return "test";
+			return prefix + "test";
+		}
+
+		public Response withHeaders() {
+			return Response.ok().header("Set-Cooike", new NewCookie.Builder("SESSION_ID").value("SID").build()).build();
 		}
 	}
 
